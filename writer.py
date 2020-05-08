53b40468f7937681b7ed144e542327896a5e7ce3
27Cobalter <kontyanzhihg@gmail.com>
2020-05-08 17:54:59 +0900

    [add] とりあえず動くように

--- writer.py
+++ writer.py
@@ -1,36 +1,100 @@
-import os
-import ctypes
-import shutil
+import datetime
 import glob
+import os
+import re
+import shutil
+import time
+
 from sys import stdout
 from struct import pack
 from zlib import crc32, compress
 
+# 本体
+class VrcMetaTool(LogToolBase):
+    config = {}
+    regex = {}
+    world = ""
+    users = []
+    date_regex = ""
 
-def chunk(name, data):
-    return pack("!I4s%dsI" % len(data), len(data), name, data, crc32(name + data))
+    def __init__(self):
+        print("Initialize!")
+        self.config["out_dir"] = "meta_pic"
+
+        self.regex["PlayerJoin"] = re.compile(
+            ".*?\[NetworkManager\] OnPlayerJoined (.*)"
+        )
+        self.regex["PlayerLeft"] = re.compile(".*?\[NetworkManager\] OnPlayerLeft (.*)")
+        self.regex["EnterRoom"] = re.compile(".*?\[RoomManager\] Entering Room: (.*)")
+        self.regex["ScreenShot"] = re.compile(".*?Took screenshot to: (.*)")
+
+        self.date_regex = re.compile(
+            ".*VRChat_[0-9]*x[0-9]*_([0-9]{4})-([0-9]{2})-([0-9]{2})_([0-9]{2})-([0-9]{2})-([0-9]{2}).([0-9]{3}).png"
+        )
+
+    def execute(self, line):
+        for event in self.regex:
+            match = self.regex[event].match(line)
+            if match:
+                print(datetime.datetime.now(), "\t" + event + ": " + match.group(1))
+                if event == "PlayerJoin":
+                    self.users.append(match.group(1))
+                elif event == "PlayerLeft":
+                    self.users.remove(match.group(1))
+                elif event == "EnterRoom":
+                    self.world = match.group(1)
+                    self.users = []
+                elif event == "ScreenShot":
+                    if not os.path.exists(match.group(1)):
+                        print(
+                            "\tError", os.path.abspath(match.group(1)), "is not found."
+                        )
+                        return
+                    date = "".join(self.date_regex.match(match.group(1)).groups())
+                    self.write(match.group(1), "".join(date))
+
+                    print(
+                        "\tWrite:",
+                        match.group(1),
+                        "\n\t\t->",
+                        os.path.abspath(
+                            os.path.join(
+                                self.config["out_dir"], os.path.basename(match.group(1))
+                            )
+                        ),
+                    )
+                    print("\t", date, self.world)
+                    print("\t", self.users)
+
+    # pngチャンク関連関数
+    def chunk(self, name, data):
+        return pack("!I4s%dsI" % len(data), len(data), name, data, crc32(name + data))
+
+    def write(self, file, date):
+        self.users.sort()
+        shutil.copy2(os.path.abspath(file), self.config["out_dir"])
+        with open(
+            os.path.join(self.config["out_dir"], os.path.basename(file)), "r+b"
+        ) as f:
+            # IEND以降を上書きする
+            f.seek(-12, 2)
+            f.write(self.chunk(b"vrCd", date.encode("utf-8")))
+            f.write(self.chunk(b"vrCw", self.world.encode("utf-8")))
+            for user in self.users:
+                f.write(self.chunk(b"vrCu", user.encode("utf-8")))
+            f.write(self.chunk(b"IEND", b""))
 
 
-def write(file, out_dir, date, world, users):
-    users.sort()
-    shutil.copy2(file, out_dir)
-    with open(os.path.join(out_dir,os.path.basename(file)), "r+b") as f:
-        # IEND以降を上書きする
-        f.seek(-12, 2)
-        f.write(chunk(b"vrCd", date))
-        f.write(chunk(b"vrCw", world))
-        for user in users:
-            f.write(chunk(b"vrCu", user))
-        f.write(chunk(b"IEND", b""))
-
+# ログツール関連共通化したい
 def select_log():
     vrc_dir = os.environ["USERPROFILE"] + "\\AppData\\LocalLow\\VRChat\\VRChat\\"
     log_files = glob.glob(vrc_dir + "output_log_*.txt")
-    log_files.sort(key = os.path.getctime, reverse = True)
+    log_files.sort(key=os.path.getctime, reverse=True)
     return log_files[0]
 
+
 def tail(thefile):
-    thefile.seek(0, 2)
+    # thefile.seek(0, 2)
     while True:
         line = thefile.readline()
         if not line:
@@ -41,21 +105,26 @@ def tail(thefile):
             yield line
 
 
+class LogToolBase:
+    def init():
+        pass
+
+    def execute(line):
+        pass
+
+
 def main():
-    out_dir = "chunk"
-    #photo_reg = re.compile()
+
     log_file = select_log()
-    with open(log_file, "rb") as f:
+
+    vrc_meta_tool = VrcMetaTool()
+
+    with open(log_file, "r", encoding="utf-8") as f:
         print("open logfile : ", log_file)
 
-    lines = tail(f)
-
-    date = b"2019-09-29_13-17-05.189"
-    world = b"Pandora ep.3"
-    users = []
-    users.append(b"bootjp")
-    users.append(b"27Cobalter")
-    write("./orig/VRChat_1920x1080_2019-09-29_13-17-05.189.png", out_dir, date, world, users)
+        lines = tail(f)
+        for line in lines:
+            vrc_meta_tool.execute(line)
 
 
 if __name__ == "__main__":
