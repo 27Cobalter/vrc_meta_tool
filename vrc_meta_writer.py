4b727b5f78a27495db8cd3f273c8bcf1c8f2da59
27Cobalter <kontyanzhihg@gmail.com>
2020-05-09 16:40:37 +0900

    [update] シンボリックリンクも考慮して比較する関数あった

--- vrc_meta_writer.py
+++ vrc_meta_writer.py
@@ -100,9 +100,7 @@ class VrcMetaTool(LogToolBase):
 
     def write(self, file, date):
         self.users.sort()
-        if os.path.dirname(os.path.abspath(file)) != os.path.abspath(
-            self.config["out_dir"]
-        ):
+        if not os.path.samefile(os.path.dirname(file), self.config["out_dir"]):
             shutil.copy2(os.path.abspath(file), self.config["out_dir"])
         with open(
             os.path.join(self.config["out_dir"], os.path.basename(file)), "r+b"
