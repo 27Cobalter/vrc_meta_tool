8e30255146c8a4a5c2318350bb130d1af52539cc
ぶーと / Yoshiaki Ueda <oh@bootjp.me>
2020-05-23 20:42:24 +0900

    add binary version

--- .github/workflows/version_embedd.py
+++ .github/workflows/version_embedd.py
@@ -1,14 +1,19 @@
 import os
+import re
 
 version = os.environ.get("GITHUB_SHA")
+version_number = []
+pattern = re.compile('.*v([0-9]+)\.([0-9]+).([0-9]+).')
+version_number = re.match(pattern, os.environ.get("GITHUB_REF")).groups()
+version_number_str = "{}.{}.{}".format(version_number[0], version_number[1], version_number[2])
 
 print(f'''
 VSVersionInfo(
   ffi=FixedFileInfo(
     # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
     # Set not needed items to zero 0.
-    # filevers=({version}),
-    # prodvers=({version}),
+    filevers=({version_number[0]}, {version_number[1]}, {version_number[2]}),
+    prodvers=({version_number[0]}, {version_number[1]}, {version_number[2]}),
     # Contains a bitmask that specifies the valid bits 'flags'r
     mask=0x17,
     # Contains a bitmask that specifies the Boolean attributes of the file.
@@ -32,12 +37,12 @@ VSVersionInfo(
         u'040904b0',
         [StringStruct(u'CompanyName', u'None'),
         StringStruct(u'FileDescription', u'vrc_meta_tool'),
-        StringStruct(u'FileVersion', u'{version}'),
+        StringStruct(u'FileVersion', u'{version_number_str}'),
         StringStruct(u'InternalName', u'vrc_meta_tool'),
         StringStruct(u'LegalCopyright', u'Copyright 2020 vrc_meta_tool. All rights reserved.'),
         StringStruct(u'OriginalFilename', u'vrc_meta_tool'),
         StringStruct(u'ProductName', u'vrc_meta_tool'),
-        StringStruct(u'ProductVersion', u'{version}'),
+        StringStruct(u'ProductVersion', u'{version_number_str}'),
         StringStruct(u'CompanyShortName', u'None'),
         StringStruct(u'ProductShortName', u'vrc_meta_tool'),
         StringStruct(u'LastChange', u'{version}'),
