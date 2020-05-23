12a8cfc46377bfb34ced2ae1c5f78585332d3b8a
ぶーと / Yoshiaki Ueda <oh@bootjp.me>
2020-05-23 20:46:08 +0900

    fix version format

--- .github/workflows/version_embedd.py
+++ .github/workflows/version_embedd.py
@@ -10,24 +10,13 @@ version_number_str = "{}.{}.{}".format(version_number[0], version_number[1], ver
 print(f'''
 VSVersionInfo(
   ffi=FixedFileInfo(
-    # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
-    # Set not needed items to zero 0.
-    filevers=({version_number[0]}, {version_number[1]}, {version_number[2]}),
-    prodvers=({version_number[0]}, {version_number[1]}, {version_number[2]}),
-    # Contains a bitmask that specifies the valid bits 'flags'r
+    filevers=({version_number[0]}, {version_number[1]}, {version_number[2]}, 0),
+    prodvers=({version_number[0]}, {version_number[1]}, {version_number[2]}, 0),
     mask=0x17,
-    # Contains a bitmask that specifies the Boolean attributes of the file.
     flags=0x0,
-    # The operating system for which this file was designed.
-    # 0x4 - NT and there is no need to change it.
     OS=0x4,
-    # The general type of file.
-    # 0x1 - the file is an application.
     fileType=0x1,
-    # The function of the file.
-    # 0x0 - the function is not defined for this fileType
     subtype=0x0,
-    # Creation date and time stamp.
     date=(0, 0)
     ),
   kids=[
